3889c3735f3448ee62a6bd94b962da7509c14f82
Cassidy G <cassidygroenendaal@gmail.com>
2019-09-09 13:09:43 -0400

    Fix overlap issue with centered items and navbar/footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function Footer(props) {
   return (
-    <footer className="footer text-center bg-light mt-auto">
+    <footer
+      className="footer text-center bg-light mt-auto"
+      style={{ position: "relative", zIndex: "999" }}
+    >
       <div className="container">
         <p>Footer</p>
       </div>
