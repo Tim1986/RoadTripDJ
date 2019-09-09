14375b2f1b854f9f58bc945c9d54f896e306d956
Cassidy G <cassidygroenendaal@gmail.com>
2019-09-08 22:49:45 -0400

    Adjusted layout of registration page

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 
-import API from '../../lib/API';
-import AuthContext from '../../contexts/AuthContext';
-import RegisterForm from '../../components/RegisterForm/RegisterForm';
+import API from "../../lib/API";
+import AuthContext from "../../contexts/AuthContext";
+import RegisterForm from "../../components/RegisterForm";
 
-import "./index.css"
+import "./index.css";
 
 class Register extends Component {
   static contextType = AuthContext;
@@ -13,20 +13,21 @@ class Register extends Component {
   state = {
     redirectToReferrer: false,
     error: ""
-  }
+  };
 
   handleSubmit = (firstName, lastName, email, password) => {
-    API.Users.register(firstName, lastName, email, password)
-      .then(response => {
-        this.setState({ redirectToReferrer: true })
+    API.Users
+      .register(firstName, lastName, email, password)
+      .then((response) => {
+        this.setState({ redirectToReferrer: true });
       })
-      .catch(err =>
-        console.log(err)
+      .catch(
+        (err) => console.log(err)
         // if (err.response.status === 401) {
         //   this.setState({ error: "Sorry, that email/password combination is not valid. Please try again." });
         // }
       );
-  }
+  };
 
   render() {
     const { from } = this.props.location.state || { from: { pathname: "/login" } };
@@ -37,23 +38,18 @@ class Register extends Component {
     }
 
     return (
-      <div className='register'>
-        <div className='row'>
-          <div className='col'>
-            <h1>Register Account</h1>
-          </div>
-        </div>
-        {this.state.error &&
-          <div className='row'>
-            <div className='col'>
-              <div className='alert alert-danger mb-3' role='alert'>
-                {this.state.error}
-              </div>
+      <div className="register container">
+        <div className="">
+          <div className="row">
+            <div className="register__container col-lg-6 col-md-9 col-12 mx-auto">
+              <h1>Register Account</h1>
+              {this.state.error && (
+                <div className="alert alert-danger mb-3" role="alert">
+                  {this.state.error}
+                </div>
+              )}
+              <RegisterForm onSubmit={this.handleSubmit} />
             </div>
-          </div>}
-        <div className='row'>
-          <div className='col'>
-            <RegisterForm onSubmit={this.handleSubmit} />
           </div>
         </div>
       </div>
