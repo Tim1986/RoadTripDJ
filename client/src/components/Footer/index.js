8b21de23539ba204d02625bf0f9cf140bd2fc1f5
anees05 <anees.adams05@gmail.com>
2019-09-09 15:07:10 -0400

    Change footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,11 +5,13 @@ import "./index.css"
 function Footer(props) {
   return (
     <footer
-      className="footer text-center mt-auto bg-dark text-light"
+      className="pt-3 footer text-center mt-auto bg-dark text-light"
       style={{ position: "relative", zIndex: "999" }}
     >
       <div className="container">
-        <p></p>
+        <p>
+          &copy; 2019 | Get In Touch
+        </p>
       </div>
     </footer>
   );
