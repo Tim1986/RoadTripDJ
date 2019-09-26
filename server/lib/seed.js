e8c6aeba73559b5670a4897b9ee188734d238134
Cassidy G <cassidygroenendaal@gmail.com>
2019-09-26 09:40:43 -0400

    Start seed function

--- server/lib/seed.js
+++ server/lib/seed.js
@@ -6,6 +6,43 @@ const seedData = require("../lib/seedData");
 
 const seed = () => {
   console.log("Seeding the database...");
+  seedData.forEach((state) => {
+    // Create each State
+    State.create(
+      {
+        name: state.name,
+        abbr: state.abbr
+      },
+      (err, newState) => {
+        if (err) console.log(err);
+        // Create each WikiCity
+        state.wikiCities.forEach((wikiCity) => {
+          WikiCity.create({ name: wikiCity.name }, (err, newWikiCity) => {
+            if (err) console.log(err);
+            // Create each artist
+            // wikiCity.artists.forEach((artist) => {
+            //   Artist.create(
+            //     {
+            //       name: artist.artist,
+            //       popularity: artist.popularity,
+            //       spotifyID: artist.spotifyID
+            //     },
+            //     (err, newArtist) => {
+            //       if (err) console.log(err);
+            //       // Puse newArtist into newWikiCity.artists
+            //       newWikiCity.artists.push(newArtist);
+            //       newWikiCity.save();
+            //     }
+            //   );
+            // });
+            // Push newWikiCity into newState.wikiCities
+            newState.wikiCities.push(newWikiCity);
+            newState.save();
+          });
+        });
+      }
+    );
+  });
 };
 
 module.exports = seed;
