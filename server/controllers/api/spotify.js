92474724bab01fcd9c6b3bc7c046439da2affd8b
Max Szczepaniak <wookslandline@protonmail.com>
2019-09-26 15:08:18 -0400

    spotifyTest can now grab track ideas for both the start and end at the same time.

--- server/controllers/api/spotify.js
+++ server/controllers/api/spotify.js
@@ -185,17 +185,44 @@ router.get("/playlist/new/:userID/:accessToken", (req, res) => {
   const userID = req.params.userID,
     accessToken = req.params.accessToken;
     console.log("API spot i fy: " + accessToken)
-  spotifyTest.grabArtists(6009402,[ 
-   '1wVEYaqTmGsl3i7np7xQjW',
-   '2qQLjtrCXMHvWF31LiQbyB',
-   '1nzDJSsUJBn9uC7LJpY5wC',
-   '0CdbG1eHVjqjkQsGoH2u1V',
-   '6jpz8XE6j7M4q5DB5Cce6I',
-   '4FXXf4RDJ6TIOX11JywHUg',
-   '6IKq5gnh3GQrnxztypZKZR',
-   '0lRHJ9PmO1uOD7LUO89KzI',
-   '3KT4jB978CkSbdqWbLgT1x',
-   '6gsEOKx8Z0CkVviR9DC3W6'], accessToken)
+  // spotifyTest.grabArtists(6009402,[
+  // { spotifyID: '1wVEYaqTmGsl3i7np7xQjW'},
+  // { spotifyID: '2qQLjtrCXMHvWF31LiQbyB'},
+  // { spotifyID: '1nzDJSsUJBn9uC7LJpY5wC'},
+  // { spotifyID: '0CdbG1eHVjqjkQsGoH2u1V'},
+  // { spotifyID: '6jpz8XE6j7M4q5DB5Cce6I'},
+  // { spotifyID: '4FXXf4RDJ6TIOX11JywHUg'},
+  // { spotifyID: '6IKq5gnh3GQrnxztypZKZR'},
+  // { spotifyID: '0lRHJ9PmO1uOD7LUO89KzI'},
+  // { spotifyID: '3KT4jB978CkSbdqWbLgT1x'},
+  // { spotifyID: '6gsEOKx8Z0CkVviR9DC3W6'}], accessToken)
+  spotifyTest.controller([
+    { spotifyID: '1wVEYaqTmGsl3i7np7xQjW'},
+    { spotifyID: '2qQLjtrCXMHvWF31LiQbyB'},
+    { spotifyID: '1nzDJSsUJBn9uC7LJpY5wC'},
+    { spotifyID: '0CdbG1eHVjqjkQsGoH2u1V'},
+    { spotifyID: '6jpz8XE6j7M4q5DB5Cce6I'},
+    { spotifyID: '4FXXf4RDJ6TIOX11JywHUg'},
+    { spotifyID: '6IKq5gnh3GQrnxztypZKZR'},
+    { spotifyID: '0lRHJ9PmO1uOD7LUO89KzI'},
+    { spotifyID: '3KT4jB978CkSbdqWbLgT1x'},
+    { spotifyID: '6gsEOKx8Z0CkVviR9DC3W6'}],
+    [{"_id":{"$oid":"5d897daa81ba0d56dff19069"},"artist":"Guerilla Black","spotifyID":"2eTcJVakGgCvZQMnw9Evbn","popularity":36.0},
+            {"_id":{"$oid":"5d897daa81ba0d56dff1906a"},"artist":"Greydon Square","spotifyID":"4ew79B4SqevHGtYGR9bY0t","popularity":36.0},
+            {"_id":{"$oid":"5d897daa81ba0d56dff1906b"},"artist":"Dem Jointz","spotifyID":"2AUT4fNQ2QC0e2f5pIxOCd","popularity":42.0},
+            {"_id":{"$oid":"5d897daa81ba0d56dff1906c"},"artist":"B.G. Knocc Out","spotifyID":"0gwtYWiJ1xK3wfDvkEWwad","popularity":36.0},
+            {"_id":{"$oid":"5d897daa81ba0d56dff1906d"},"artist":"CPO Boss Hogg","spotifyID":"5vKNduT1XEQtCsdTPlVPkT","popularity":0.0},
+            {"_id":{"$oid":"5d897daa81ba0d56dff1906e"},"artist":"Dr. Dre","spotifyID":"6DPYiyq5kWVQS4RGwxzPC7","popularity":79.0},
+            {"_id":{"$oid":"5d897daa81ba0d56dff1906f"},"artist":"Nadine Conner","spotifyID":"3f5YSIVw4Ggkn3r2ygcNvD","popularity":4.0},
+            {"_id":{"$oid":"5d897daa81ba0d56dff19070"},"artist":"Big Fase 100","spotifyID":"3bUQeZP9mKBwS8c3Zdturd","popularity":7.0},
+            {"_id":{"$oid":"5d897daa81ba0d56dff19071"},"artist":"Coolio","spotifyID":"3y24n3XhZ96wgwRXjvS17T","popularity":66.0},
+            {"_id":{"$oid":"5d897daa81ba0d56dff19072"},"artist":"Tha Chill","spotifyID":"7vPLHiyrVGx5OmaSPqJNGw","popularity":24.0},
+            {"_id":{"$oid":"5d897daa81ba0d56dff19073"},"artist":"Hot Dollar","spotifyID":"4rfcl5vW46isXf0TUI5lIx","popularity":16.0},
+            {"_id":{"$oid":"5d897daa81ba0d56dff19074"},"artist":"2nd II None","spotifyID":"1rlHVdoyfG6pQkzcMWI4dr","popularity":37.0},
+            {"_id":{"$oid":"5d897daa81ba0d56dff19075"},"artist":"Ernie C","spotifyID":"1nR2LD5frbLYXkrJiFItKt","popularity":0.0},
+            {"_id":{"$oid":"5d897daa81ba0d56dff19076"},"artist":"Dresta","spotifyID":"4kf7SKmUMiNmNvPgV9Uncb","popularity":29.0},
+            {"_id":{"$oid":"5d897daa81ba0d56dff19077"},"artist":"MC Ren","spotifyID":"4fbyGOEjViyE97W58eGEgU","popularity":54.0},
+            {"_id":{"$oid":"5d897daa81ba0d56dff19078"},"artist":"The Fixxers","spotifyID":"5OoN9Ifb912te3rnAkXISP","popularity":23.0}], accessToken)
   
   // axios({
   //   url: `https://api.spotify.com/v1/users/${userID}/playlists`,
