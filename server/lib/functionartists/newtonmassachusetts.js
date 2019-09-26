c9f0e1045f5cdd162d4e78c1c0333c9e62c64c9a
anees05 <anees.adams05@gmail.com>
2019-09-26 15:45:31 -0400

    Add artists for the first 25 states

---
+++ server/lib/functionartists/newtonmassachusetts.js
@@ -0,0 +1,58 @@
+const newtonMAArtists = [{
+    artist: 'Robert Beaser',
+    spotifyID: '71PWbdkiG74gueYmaEeFxN',
+    popularity: 2
+},
+{
+    artist: 'Frederick Converse',
+    spotifyID: '1dqBUpT3J583rOtDGXZ3MO',
+    popularity: 0
+},
+{
+    artist: 'Ralph Burns',
+    spotifyID: '3kt1xo56PyGb13t2OCf08h',
+    popularity: 21
+},
+{
+    artist: 'Avi Jacob',
+    spotifyID: '28iPQ3ZebapNRuZhw2Agvr',
+    popularity: 3
+},
+{
+    artist: 'Mark Sandman',
+    spotifyID: '0bFmF1Nq7ezl0rjjBZExCK',
+    popularity: 21
+},
+{
+    artist: 'Rachel Platten',
+    spotifyID: '3QLIkT4rD2FMusaqmkepbq',
+    popularity: 68
+},
+{
+    artist: 'Robert Morse',
+    spotifyID: '3hPhlz7jnxSac3ytoRc3FB',
+    popularity: 15
+},
+{
+    artist: 'Horatio Parker',
+    spotifyID: '6UTSsPo4Iu1XrRJLOXwCeY',
+    popularity: 1
+},
+{
+    artist: 'Jack Lemmon',
+    spotifyID: '7Iqm4p7WbS3za8z9GnRe3o',
+    popularity: 2
+},
+{
+    artist: 'Harold Shapero',
+    spotifyID: '16LnAvr7EOy7d7l65yvi6w',
+    popularity: 3
+},
+{
+    artist: 'Fritz Richmond',
+    spotifyID: '6GfGGJPlsEon62hudBP3jg',
+    popularity: 3
+}
+]
+
+module.exports = newtonMAArtists;
